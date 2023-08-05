439f6f558a49cab29e066e23362faf4300af9757
Rahim_Badsa <rahimbadsa723@gmail.com>
2023-08-05 17:15:31 +0600

    min max finder with if else

--- basics/max and min nunber finder.js
+++ basics/max and min nunber finder.js
@@ -1,6 +1,6 @@
-let a = 7;
-let b = 2;
-let c = 9;
+let a = 898;
+let b = 78;
+let c = 3;
 
 
 //with if else
@@ -8,9 +8,20 @@ function findmax(a, b, c){
       if(a>b && a>c){
             console.log('a is big than b c');
       }else if(b>a && b>c){
-            console.log('b is big than b c');
+            console.log('b is big than a c');
       }else if(c>b && c>a){
-            console.log('c is big than b c');
+            console.log('c is big than b a');
       }
 }
-findmax(a, b, c)
+
+function findmin(a, b, c){
+      if(a<b && a<c){
+            console.log('a is small than b c');
+      }else if(b<a && b<c){
+            console.log('b is small than a c');
+      }else if(c<b && c<a){
+            console.log('c is small than b a');
+      }
+}
+findmax(a, b, c);
+findmin(a, b, c);
