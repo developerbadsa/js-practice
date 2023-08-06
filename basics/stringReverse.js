84158f9432c4bcc112ac2182a48c64d99009fa1a
Rahim_Badsa <rahimbadsa723@gmail.com>
2023-08-06 09:24:16 +0600

    error on reversed string

---
+++ basics/stringReverse.js
@@ -0,0 +1,13 @@
+let str = 'i am developer';
+
+function makeReverse(){
+      var reversed
+      for(let i = str.length+1;i > 0;i--){
+
+          var reversed = reversed + str[i];
+
+            
+      }
+      console.log(reversed);
+}
+makeReverse()
